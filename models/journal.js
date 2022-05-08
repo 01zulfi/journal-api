0b6b93ac6dc3a651bde8e4c06c910a09fb1c8426
01zulfi <85733202+01zulfi@users.noreply.github.com>
2022-05-08 18:15:35 +0500

    Add journal.js model

---
+++ models/journal.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const JournalSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    content: { type: String },
+    date: { type: String },
+    publish: { type: Boolean, required: true },
+    author: { type: Schema.ObjectId, ref: 'User' },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+module.exports = mongoose.model('Journal', JournalSchema);
