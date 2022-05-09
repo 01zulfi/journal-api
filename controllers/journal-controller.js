85d87405414c9972003c362790e58b902fc87aa8
01zulfi <85733202+01zulfi@users.noreply.github.com>
2022-05-09 18:26:37 +0500

    Complete getJournals function

--- controllers/journal-controller.js
+++ controllers/journal-controller.js
@@ -1,5 +1,20 @@
+const Journal = require('../models/journal');
+
 exports.getJournals = (req, res, next) => {
-  res.status(200).json({ message: 'get journals wohoo' });
+  Journal.find({})
+    .sort({ createdAt: -1 })
+    .exec((err, result) => {
+      if (err) {
+        return res.status(500).json({ message: 'Error while fetching data.' });
+      }
+      if (result.length === 0) {
+        return res.status(204).json({ message: 'No journals in database.' });
+      }
+      return res.status(200).json({
+        message: 'Ok',
+        journals: result,
+      });
+    });
 };
 
 exports.createJournal = (req, res, next) => {
