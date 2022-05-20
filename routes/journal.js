4bf53f28328200fc86c52414163078dfa53342b6
01zulfi <85733202+01zulfi@users.noreply.github.com>
2022-05-20 15:29:07 +0500

    Fix controller name in journal router

--- routes/journal.js
+++ routes/journal.js
@@ -4,7 +4,7 @@ const controller = require('../controllers/journal-controller');
 const router = express.Router();
 
 router.get('/', controller.getJournals);
-router.get('/:id', controller.getOneJournal);
+router.get('/:id', controller.getJournalById);
 router.post('/', controller.createJournal);
 router.put('/:id', controller.updateJournal);
 router.delete('/:id', controller.deleteJournal);
